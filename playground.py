639f5a2c709ca5047f2c1a6875d73b8d74beaab8
looka <channel.tubeluka@gmail.com>
2023-09-13 23:41:39 +0200

    Update playground.py

--- playground.py
+++ playground.py
@@ -1,11 +1,1 @@
-import random
-numbers = [random.randrange(start=1, stop=17) for _ in range(8)]
-
-a = int(input(f'What is {numbers[1]} X {numbers[2]}'))
-b = int(input(f'What is {numbers[2]} X {numbers[2]}'))
-c = int(input(f'What is {numbers[3]} X {numbers[2]}'))
-d = int(input(f'What is {numbers[1]} X {numbers[2]}'))
-
-
-if a == a*b:
-    a1 = 100
+a1 = 100
